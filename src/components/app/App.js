47c4e2569d91210236098dab50036303558f0e7f
Max Ivanov <waxivanov@gmail.com>
2022-07-20 23:50:13 +0300

    added filter logic

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,47 @@
-import Header from "../header/Header";
-import Menu from "../menu/Menu";
-import OrderHistory from "../order-history/OrderHistory";
+import React from 'react';
+import Header from '../header/Header';
+import Menu from '../menu/Menu';
+import OrderHistory from '../order-history/OrderHistory';
+import Filter from '../filter/Filter';
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <hr />
-            <OrderHistory />
-            <hr />
-            <Menu />
-        </div>
-    );
+import orderHistoryData from '../../json/order-history.json';
+import menuData from '../../json/menu.json';
+
+class App extends React.Component {
+    state = {
+        filter: '',
+        orderHistory: orderHistoryData,
+        menu: menuData,
+    };
+
+    handleFilterChange = (e) => {
+        this.setState({
+            filter: e.target.value,
+        });
+    };
+
+    filterMenu = (menu, filter) => {
+        return menu.filter((item) =>
+            item.name.toLowerCase().includes(filter.toLowerCase())
+        );
+    };
+
+    render() {
+        const { orderHistory, menu, filter } = this.state;
+
+        const filteredMenu = this.filterMenu(menu, filter);
+
+        return (
+            <div>
+                <Header />
+                <hr />
+                <OrderHistory orderHistory={orderHistory} />
+                <hr />
+                <Filter filter={filter} onChange={this.handleFilterChange} />
+                <Menu menu={filteredMenu} />
+            </div>
+        );
+    }
 }
 
 export default App;
