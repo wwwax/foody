da604daaebdbfe285471dab37bf30b0a89d95071
Max Ivanov <waxivanov@gmail.com>
2022-08-02 00:31:58 +0300

    dropdown-toggle

--- src/components/user-menu/UserMenu.js
+++ src/components/user-menu/UserMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 import avatar from "../../images/avatar.png";
 import Dropdown from "../dropdown/Dropdown";
 
@@ -9,18 +9,39 @@ class UserMenu extends React.Component {
         isDropdownOpen: false,
     };
 
+    containerRef = createRef();
+
     toggleDropdown = () => {
         this.setState((prev) => ({
             isDropdownOpen: !prev.isDropdownOpen,
         }));
     };
 
+    handleClick = (e) => {
+        if (!this.containerRef.current.contains(e.target)) {
+            this.setState({ isDropdownOpen: false });
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener("click", this.handleClick);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("click", this.handleClick);
+    }
+
     render() {
         const { isDropdownOpen } = this.state;
         const { width } = this.props;
 
         return (
-            <div className="UserMenu" onClick={this.toggleDropdown}>
+            <div
+                style={{ border: "1px solid red" }}
+                className="UserMenu"
+                onClick={this.toggleDropdown}
+                ref={this.containerRef}
+            >
                 <img width={width} src={avatar} alt="avatar" />
                 <span className="UserName">{name}</span>
                 {isDropdownOpen && <Dropdown />}
